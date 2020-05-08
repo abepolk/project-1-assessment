be577b92a4ca37d7d3c82dfd1b4b2eeb1b000da3
Abraham Polk <abepolk@gmail.com>
2020-05-08 11:10:23 -0400

    JS app finished

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log('connected');
+
+// Also decrements
+const increment = (sign) => {
+    interval = parseInt($('#interval-input').val());
+    oldVal = parseInt($('h1').text());
+    let newVal;
+    if (sign === "plus") {
+        newVal = oldVal + interval;
+    }
+    else if (sign === "minus") {
+        newVal = oldVal - interval;
+    } else {
+        console.error("Operation error");
+    }
+    $('h1').text(newVal.toString());
+}
+
+$('#plus-button').on('click', function(event) {
+    increment("plus");
+});
+
+$('#minus-button').on('click', function(event) {
+    increment("minus");
+});
